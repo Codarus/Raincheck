fd1ca4baf524f323cf3e395108f71f8f2ee227f1
Christopher Hernandez <cherna11@hawk.iit.edu>
2017-09-04 16:11:32 -0500

    added custom naming functionality and changed UI accordingly

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ Repository for the Raincheck Google Chrome app
 This app collects and labels currently open URLs, organizes them into a uniquely named text file, and downloads the file onto your machine.
 This allows for the instant "bookmarking" of several pages which a user may then browse at their leisure.
 
+#Update 9/4/2017#
+*Added custom name fuctionality
+*Changed UI to support custom naming
+*cleaned up code
+
 #TODO#
 
-Add custom name functionality
-Add settings page
-
-test
+Add features & add settings page
--- popup.js
+++ popup.js
@@ -3,19 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
 	//get tab urls
 	chrome.tabs.query({ currentWindow: true }, function (tabs) {
 		
-		var text = "";
-		var i = 0;
-		
-		while (i < tabs.length){
-			if (i != tabs.length-1){
-				text += tabs[i].title + ": " + "\r\n" + tabs[i].url + "\r\n\r\n";
-			}
-			else{
-				text += tabs[i].title + ": " + "\r\n" + tabs[i].url;
-			}
-			i++;
-		}
-		
 		//set date for unique download identifier
 		var monthNames = [
         "01", "02", "03",
@@ -30,16 +17,41 @@ document.addEventListener('DOMContentLoaded', function () {
 		var hour = date.getHours();
 		var minutes = date.getMinutes();
 		var seconds = date.getSeconds();
+		var name = "raincheck-"+year+"-"+day+"-"+monthNames[monthIndex]+"-"+hour+"-"+minutes+"-"+seconds;
+		//set chrome tabs api variables
+		var text = "";
+		var i = 0;
+		//instantiate custom name & buttons
+		var name_input = null;
+		var btn = document.getElementById("btn");
 		
+		//get & store tab data
+		while (i < tabs.length){
+			if (i != tabs.length-1){
+				text += tabs[i].title + ": " + "\r\n" + tabs[i].url + "\r\n\r\n";
+			}
+			else{
+				text += tabs[i].title + ": " + "\r\n" + tabs[i].url;
+			}
+			i++;
+		}
+		
+		//fix military time
 		if (day<10){day = "0" + day};
 		
 		if (hour > 12){
         hour = hour - 12;
 		}
 		
-		var name = "raincheck-"+year+"-"+day+"-"+monthNames[monthIndex]+"-"+hour+"-"+minutes+"-"+seconds;
-
-		download(name,text);
+		//set download to start after user input validation & button press
+		btn.addEventListener("click", function(){
+			name_input = $("input").val();
+			if(name_input != null && /^[a-zA-Z0-9-_ ]*$/.test(name_input) != false && /\S/.test(name_input) != false){
+				name = name_input;
+			}
+			
+			download(name,text);
+		});
 			
 	});
 		
